468612b5bd0b6ae5598306a4c6c6fb1705b35ea1
justundertaker <806792561@qq.com>
2023-03-10 01:35:20 +0800

    添加action模型字典

---
+++ wechatbot_client/wechat/action_check.py
@@ -0,0 +1,81 @@
+from typing import Type
+
+from pydantic import BaseModel, create_model
+
+from wechatbot_client.com_wechat import ComWechatApi
+
+from .utils import get_typed_annotation, get_typed_signature
+
+ACTION_LIST = [
+    "send_text",
+    "send_image",
+    "send_file",
+    "send_xml",
+    "send_card",
+    "send_at_msg",
+    "get_self_info",
+    "get_contacts",
+    "get_friend_list",
+    "get_chatroom_list",
+    "get_official_account_list",
+    "get_friend_by_remark",
+    "get_friend_by_wxid",
+    "get_friend_by_nickname",
+    "get_user_info",
+    "get_group_members",
+    "check_friend_status",
+    "start_receive_message",
+    "stop_receice_message",
+    "get_db_handles",
+    "execute_sql",
+    "backup_db",
+    "verify_friend_apply",
+    "add_friend_by_wxid",
+    "add_friend_by_v3",
+    "get_wechat_version",
+    "search_user_info",
+    "follow_public_number",
+    "change_wechat_version",
+    "delete_user",
+    "send_app_msg",
+    "edit_remark",
+    "set_group_name",
+    "set_group_announcement",
+    "set_group_nickname",
+    "get_groupmember_nickname",
+    "delete_groupmember",
+    "add_groupmember",
+    "open_browser",
+    "get_history_public_msg",
+    "forward_msg",
+    "get_qrcode_image",
+    "get_a8key",
+    "send_origin_xml",
+    "logout",
+    "get_transfer",
+    "send_gif_msg",
+    "get_msg_cdn",
+]
+"""action列表"""
+
+ACTION_DICT: dict[str, Type[BaseModel]] = {}
+"""action模型字典"""
+
+
+def gen_action_dict():
+    """
+    生成action字典
+    """
+    global ACTION_DICT
+    for action in ACTION_LIST:
+        call = getattr(ComWechatApi, action)
+        signature = get_typed_signature(call)
+        field = {}
+        globalns = getattr(call, "__globals__", {})
+        for parameter in signature.parameters.values():
+            name = parameter.name
+            annotation = get_typed_annotation(parameter, globalns)
+            if name != "self":
+                field[name] = (annotation, ...)
+        action_type = create_model(action, **field)
+        ACTION_DICT[action] = action_type
---
+++ wechatbot_client/wechat/utils.py
@@ -0,0 +1,37 @@
+import inspect
+from typing import Any, Callable, ForwardRef
+
+from pydantic.typing import evaluate_forwardref
+
+from wechatbot_client.log import logger
+
+
+def get_typed_signature(call: Callable[..., Any]) -> inspect.Signature:
+    """获取可调用对象签名"""
+    signature = inspect.signature(call)
+    globalns = getattr(call, "__globals__", {})
+    typed_params = [
+        inspect.Parameter(
+            name=param.name,
+            kind=param.kind,
+            default=param.default,
+            annotation=get_typed_annotation(param, globalns),
+        )
+        for param in signature.parameters.values()
+    ]
+    return inspect.Signature(typed_params)
+
+
+def get_typed_annotation(param: inspect.Parameter, globalns: dict[str, Any]) -> Any:
+    """获取参数的类型注解"""
+    annotation = param.annotation
+    if isinstance(annotation, str):
+        annotation = ForwardRef(annotation)
+        try:
+            annotation = evaluate_forwardref(annotation, globalns, globalns)
+        except Exception as e:
+            logger.opt(colors=True, exception=e).warning(
+                f'Unknown ForwardRef["{param.annotation}"] for parameter {param.name}'
+            )
+            return inspect.Parameter.empty
+    return annotation
